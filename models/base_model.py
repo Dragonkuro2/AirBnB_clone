1c994e35f0d9ff0609466459ff124a0e1e1f4dec
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-01-09 12:21:20 -0800

    we removed updated_at instance so I create it again

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
                 created_at: it has the current datetime.
                 updated_at: it update the current dattime. """
 
-
+        self.updated_at = datetime.datetime.now()
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.today()
