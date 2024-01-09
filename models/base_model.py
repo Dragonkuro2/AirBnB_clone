a1ad4f9fee30d53d6de99db63ce55a1a3fc1e8f6
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-01-09 07:44:18 -0800

    the basemodel class with empty folder for the tests

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" BaseModel file. """
+import uuid
+import datetime
+
+
+class BaseModel:
+    """ a class BaseModel that defines all common attributes/methods
+        for other classes. """
+    def __init__(self, *args, **kwargs):
+        """ initialize the elements.
+            args:
+                id: it has the id generated using uuid version 4.
+                created_at: it has the current datetime.
+                updated_at: it update the current dattime. """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.today()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """ return this format '[<class name>] (<self.id>) <self.__dict__>'"""
+        cls = self.__class__.__name__
+        return f"[{cls}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """ update the datetime with the current datetime. """
+        self.updated_at = datetime.datetime.today()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values
+        of __dict__ of the instance. """
+        r_dict = self.__dict__.copy()
+        r_dict['__class__'] = self.__class__.__name__
+        r_dict['created_at'] = self.created_at.isoformat()
+        r_dict['updated_at'] = self.updated_at.isoformat()
+        return r_dict
