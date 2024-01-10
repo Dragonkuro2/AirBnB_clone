d064012b42f2e214be5f1b35bd0d9b9238a2f5a6
Aissamab <aissamabakkar19@@gmail.com>
2024-01-10 11:35:16 -0800

    your filestorage methodes doesnt work

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -9,6 +9,9 @@ class HBNBCommand(cmd.Cmd):
 
     """
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+    }
 
 
     def do_quit(self, arg):
@@ -26,6 +29,17 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Do nothing when cmd erceive an empty line """
         pass
+
+    def do_create(self, arg):
+        """ Create command to create a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
 
 
 if __name__ == '__main__':
